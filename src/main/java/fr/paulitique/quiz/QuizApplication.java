fab536fa649ffa8f0990ce3d38dc3a98852c03dc
Dorian OUAKLI <ouaklidori@cy-tech.fr>
2022-11-18 15:23:06 +0100

    Add swagger2

--- src/main/java/fr/paulitique/quiz/QuizApplication.java
+++ src/main/java/fr/paulitique/quiz/QuizApplication.java
@@ -3,13 +3,14 @@ package fr.paulitique.quiz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class QuizApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizApplication.class, args);
 	}
-	
-	
 
 }
