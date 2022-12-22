ca0860ac171e2114c8cc4cc2e5e7d6b58966ee81
lostsh <yohann.vernhes@gmail.com>
2022-12-22 11:57:29 +0100

    answering questions is now available

--- src/main/java/fr/paulitique/quiz/view/QuizWebAnswerController.java
+++ src/main/java/fr/paulitique/quiz/view/QuizWebAnswerController.java
@@ -91,8 +91,7 @@ public class QuizWebAnswerController {
 				model.addAttribute("status", "Quiz empty");
 			}else {
 				model.addAttribute("status", "Quiz finished");
-			}
-			
+			}			
 			
 		}else if(numQuestion.equals("")) {
 			if(questionList.size()>0) {
@@ -105,110 +104,70 @@ public class QuizWebAnswerController {
 			}
 		} else {
 			int num = Integer.parseInt(numCurrentQuestion);
-			if(move.equals("forward") && num >= 0 && num < questionList.size()) {
-				numCurrentQuestion = (num+1)+"";
-				currentQuestion = questionList.get(num);
-			}else {
-				return "redirect:/answer/quiz?idQuiz="+idQuiz+"&numQuestion=-1";
-			}
+			
 			if(move.equals("backward") && num >= 0 && num < questionList.size()) {
 				numCurrentQuestion = (num-1)+"";
 				currentQuestion = questionList.get(num);
 			}
+			
+			if(move.equals("forward") && num >= 0 && num < questionList.size()) {
+				numCurrentQuestion = (num+1)+"";
+				currentQuestion = questionList.get(num);
+			}else {
+				if (!answer.equals("")) {
+					UriComponents postUri = UriComponentsBuilder.fromHttpUrl(API_ENDPONIT).path("/quiz/").path(idQuiz)
+							.path("/question/").path(idQuestion).path("/answer").build().encode();
+					String jsonQuestion = "";
+					Map<String, Object> jsonMap = new HashMap<String, Object>();
+					jsonMap.put("answerType", "FreeAnswer");
+					jsonMap.put("text", answer);
+					try {
+						jsonQuestion = new ObjectMapper().writeValueAsString(jsonMap);
+					} catch (JsonProcessingException e) {
+						e.printStackTrace();
+					}
+					HttpHeaders h = new HttpHeaders();
+					h.setContentType(MediaType.APPLICATION_JSON);
+					new RestTemplate().postForObject(postUri.toUriString(), new HttpEntity<String>(jsonQuestion, h),
+							String.class);
+
+				}
+				return "redirect:/answer/quiz?idQuiz="+idQuiz+"&numQuestion=-1";
+			}
+
 		}
+		
+		if(!answer.equals("")) {
+						
+			UriComponents postUri = UriComponentsBuilder
+					.fromHttpUrl(API_ENDPONIT)
+					.path("/quiz/")
+					.path(idQuiz)
+					.path("/question/")
+					.path(idQuestion)
+					.path("/answer")
+					.build().encode();
+
+			String jsonQuestion = "";
+			Map<String, Object> jsonMap = new HashMap<String, Object>();
+			jsonMap.put("answerType", "FreeAnswer");
+			jsonMap.put("text", answer);
+			try {
+				jsonQuestion = new ObjectMapper().writeValueAsString(jsonMap);
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+			}
+			HttpHeaders h = new HttpHeaders();
+			h.setContentType(MediaType.APPLICATION_JSON);
+			new RestTemplate().postForObject(postUri.toUriString(), new HttpEntity<String>(jsonQuestion, h), String.class);
+		}
+		
 		
 		// display the current question
 		model.addAttribute("currentQuestion", currentQuestion);
 		model.addAttribute("numCurrentQuestion", numCurrentQuestion);
 		
 		return "answerQuiz";
-	}
-	
-	/*
-	@GetMapping("/answer/quiz")
-	public String answerQuiz(
-			@RequestParam(name="idQuiz", required=true) String idQuiz,
-			@RequestParam(name="numQuestion", required=false, defaultValue="") String numQuestion,
-			@RequestParam(name="move", required=false, defaultValue="") String move,
-			@RequestParam(name="idQestion", required=false, defaultValue="") String idQuestion,
-			@RequestParam(name="answer", required=false, defaultValue="") String answer,
-			@RequestParam(name="status", required=false) String status,
-			Model model) {
-		
-		UriComponents uri = UriComponentsBuilder
-				.fromHttpUrl(API_ENDPONIT)
-				.path("/quiz/")
-				.path(idQuiz)
-				.build().encode();
-		
-		Object res = new RestTemplate().getForObject(uri.toUriString(), Object.class);
-		model.addAttribute("quiz", res);
-		
-		
-		UriComponents questionsUri = UriComponentsBuilder
-				.fromHttpUrl(API_ENDPONIT)
-				.path("/quiz/")
-				.path(idQuiz)
-				.path("/question/all")
-				.build().encode();
-		
-		ResponseEntity<QuestionDTO[]> questions = new RestTemplate().getForEntity(questionsUri.toUriString(), QuestionDTO[].class);
-		
-		ArrayList<QuestionDTO> questionList = new ArrayList<QuestionDTO>(Arrays.asList(questions.getBody()));
-		//questionList.sort((q1, q2) -> q1.getId().compareTo(q2.getId()));
-		
-		
-		QuestionDTO currentQuestion = null;
-		
-		if(numQuestion.equals("-1")) {
-			// validate the whole answers
-			// check if there is more than zero questions
-			// current question set to null and show validation buttons instead
-			model.addAttribute("status", "test?");
-			return "redirect:/answer/work";
-		}else if(numQuestion.equals("")){
-			// get the first question of the list
-			if(questionList.size()>0) {
-				currentQuestion = questionList.get(0);
-			}else {
-				return "redirect:/answer/quiz?idQuiz="+idQuiz+"&numQuestion=-1";
-			}
-		}
-		
-		int numCurrentQuestion = 0;
-		
-		if(move.equals("forward")){
-			numCurrentQuestion = Integer.parseInt(numQuestion)+1;
-			System.out.println("num question : "+numCurrentQuestion);
-			if(questionList.size()>0 && numCurrentQuestion >= 0 && numCurrentQuestion < questionList.size()) {
-				currentQuestion = questionList.get(numCurrentQuestion);
-				System.out.println("increment");
-			}else {
-				System.out.println("conditions doesn't work");
-				return "redirect:/answer/quiz?idQuiz="+idQuiz+"&numQuestion=-1";
-			}
-			System.out.println("Forward worked");
-		}
-		
-		System.out.println("final um question : "+((numCurrentQuestion==0)?numQuestion:numCurrentQuestion));
-		model.addAttribute("numQuestion", (numCurrentQuestion==0)?numQuestion:numCurrentQuestion);
-		// display the current question
-		model.addAttribute("currentQuestion", currentQuestion);
-		
-		return "answerQuiz";
-	}*/
-	
-	@GetMapping("/answer/quiz/question")
-	public String answerQuizQuestion(
-			@RequestParam(name="idQuiz", required=true) String idQuiz,
-			@RequestParam(name="numQuestion", required=false, defaultValue="") String numQuestion,
-			@RequestParam(name="move", required=false, defaultValue="") String move,
-			@RequestParam(name="idQestion", required=false, defaultValue="") String idQuestion,
-			@RequestParam(name="answer", required=false, defaultValue="") String answer,
-			@RequestParam(name="status", required=false) String status,
-			Model model) {
-		
-		return "redirect:/answer/quiz?idQuiz=\"+idQuiz+\"&numQuestion=-1";
 	}
 
 }
